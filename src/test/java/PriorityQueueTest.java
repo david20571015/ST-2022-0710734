48caed214d66c4a1c780c28ea6fe3a69775f12e8
David, Chiu <david20571015@gmail.com>
2022-03-12 03:36:16 +0800

    fix: test

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -13,11 +13,11 @@ public class PriorityQueueTest {
 
   private static Stream<Arguments> streamProvider() {
     return Stream.of(
-        Arguments.of(new int[] {3, 2, 2}, new int[] {1, 2, 3}),
-        Arguments.of(new int[] {-2, -1, -2, 5}, new int[] {-3, -2, -1, 5}),
-        Arguments.of(new int[] {3, -2, -4, -1, 2}, new int[] {-5, -2, -1, 2, 3}),
-        Arguments.of(new int[] {-2, 1, 11, 0, 9, 3}, new int[] {-3, 0, 1, 3, 9, 11}),
-        Arguments.of(new int[] {3, 7, 2, -1, -1}, new int[] {-2, -1, 2, 3, 7}));
+        Arguments.of(new int[] {3, 1, 2}, new int[] {1, 2, 3}),
+        Arguments.of(new int[] {-3, -1, -2, 5}, new int[] {-3, -2, -1, 5}),
+        Arguments.of(new int[] {3, -2, -5, -1, 2}, new int[] {-5, -2, -1, 2, 3}),
+        Arguments.of(new int[] {-3, 1, 11, 0, 9, 3}, new int[] {-3, 0, 1, 3, 9, 11}),
+        Arguments.of(new int[] {3, 7, 2, -1, -2}, new int[] {-2, -1, 2, 3, 7}));
   }
 
   @ParameterizedTest(name = "#{index} - Test with Argument={0}, {1}")
